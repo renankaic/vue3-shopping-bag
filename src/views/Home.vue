9c6f8d493e51d0cecaae3981ff1ed3d2ec58c162
Renan Kaic Lopes <renan-kaic@outlook.com>
2021-10-27 20:44:03 -0300

    Feat - Using MapState and adding confirm() JS dialog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,17 +21,15 @@
 </template>
 
 <script>
-
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
-  computed: {
-    products() {
-      return this.$store.state.products
-    },
-    productsInBag() {
-      return this.$store.state.productsInBag
-    }
-  },
+  computed: 
+    mapState([
+      'products',
+      'productsInBag'
+    ])
+  ,
   methods: {
    addToBag(product) {
     product.quantity = 1
