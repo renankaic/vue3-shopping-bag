465fedfd04c6a179a369498d178276e609336cde
Renan Kaic Lopes <renan-kaic@outlook.com>
2021-10-27 20:18:23 -0300

    Feat - Checking whether product is in bag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
-    <div class="products">
-      <div 
+    <div class="products" v-if="this.products.length">
+      <div         
         v-for="(product, index) in this.products"
         :key="index"
-        class="product">
+        class="product"
+        :class="{ inBag : isInBag(product) }"
+        >
           <div class="product-image" :style="{ backgroundImage: 'url(' + product.image + ')' }"></div>
           <h4>{{ product.title}}</h4>
           <p class="price">R$ {{ product.price.toFixed(2) }}</p>
-          <button @click="addToBag(product)">Adicionar ao carrinho</button>
+          <button v-if="!isInBag(product)" @click="addToBag(product)">Adicionar ao carrinho</button>
+          <button v-else class="remove" >Remove from bag</button>
       </div>
     </div>
   </div>
@@ -30,6 +33,9 @@ export default {
    addToBag(product) {
     product.quantity = 1
     this.$store.dispatch('addToBag', product)
+   },
+   isInBag(product) {
+     return this.productsInBag.find(prod => prod.id === product.id)
    }
   }
 }
